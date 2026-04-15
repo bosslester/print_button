3288acfbfb9dea07445f8b6502d566a0c5ba5a40
bosslester <musteralesteremil@gmail.com>
2026-04-15 10:52:14 +0800

    amat to check

--- src/app/amateur/amateur.component.ts
+++ src/app/amateur/amateur.component.ts
@@ -1,12 +1,31 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-amateur',
   standalone: true,
+  imports: [FormsModule],
   templateUrl: './amateur.component.html',
   styleUrls: ['./amateur.component.css']
 })
 export class AmateurComponent {
+  formData = {
+    licensee: '',
+    callSign: '',
+    licenseClass: '',
+    equipmentOne: '',
+    equipmentTwo: '',
+    address: '',
+    stationLocation: '',
+    validUntil: '',
+    issuedOn: '',
+    officialReceipt: '',
+    amount: '',
+    datePaid: '',
+    cashier: '',
+    footerLineOne: '',
+    footerLineTwo: ''
+  };
 
   print() {
     window.print();
